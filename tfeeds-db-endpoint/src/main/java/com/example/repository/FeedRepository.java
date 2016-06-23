16441f0336b653bbdb900785fe8c6209a3dd1480
neostrange <ali.hur57@gmail.com>
2016-06-23 18:44:18 +0500

    Added repository method.

--- tfeeds-db-endpoint/src/main/java/com/example/repository/FeedRepository.java
+++ tfeeds-db-endpoint/src/main/java/com/example/repository/FeedRepository.java
@@ -1,13 +1,20 @@
 package com.example.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.example.model.Feed;
 
 
 @RepositoryRestResource(collectionResourceRel="feed", path = "feed")
-public interface FeedRepository extends CrudRepository<Feed, String> {
+public interface FeedRepository extends JpaRepository<Feed, String> {
+
+	List<Feed> findByType(@Param("type") String type);
+	
+	List<Feed> findByTypeAndFirstSeen(@Param("type") String type, @Param("firstSeen") String firstSeen);
 
 }
