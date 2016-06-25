44a172587c8f2eb1b3e5608edbf03ed3e78a996e
neostrange <ali.hur57@gmail.com>
2016-06-25 16:17:10 +0500

    Added more queries.

--- tfeeds-db-endpoint/src/main/java/com/example/repository/FeedRepository.java
+++ tfeeds-db-endpoint/src/main/java/com/example/repository/FeedRepository.java
@@ -3,8 +3,6 @@ package com.example.repository;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import javax.ws.rs.QueryParam;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -19,6 +17,16 @@ public interface FeedRepository extends JpaRepository<Feed, String> {
 	List<Feed> findByType(@Param("type") String type);
 
 	List<Feed> findByTypeAndFirstSeen(@Param("type") String type, @Param("firstSeen") String firstSeen);
+
+	// find by incident type and threat type
+	List<Feed> findByTypeAndThreatType_Malware(@Param("type") String type, @Param("isMalware") boolean value);
+	List<Feed> findByTypeAndThreatType_Web(@Param("type") String type, @Param("isWeb") boolean value);
+	List<Feed> findByTypeAndThreatType_BruteForce(@Param("type") String type, @Param("isBruteForce") boolean value);
+	List<Feed> findByTypeAndThreatType_Sip(@Param("type") String type, @Param("isSip") boolean value);
+	List<Feed> findByTypeAndThreatType_PossibleCompromise(@Param("type") String type, @Param("isPossibleCompromise") boolean value);
+	List<Feed> findByTypeAndThreatType_Db(@Param("type") String type, @Param("isDb") boolean value);
+	List<Feed> findByTypeAndThreatType_Recon(@Param("type") String type, @Param("isRecon") boolean value);
+	
 	
 	List<Feed> findByThreatType_BruteForce(@Param("isBruteForce") boolean type);
 	
@@ -29,6 +37,11 @@ public interface FeedRepository extends JpaRepository<Feed, String> {
 	List<Feed> findByThreatType_Db(@Param("isDb") boolean type);
 	List<Feed> findByThreatType_Recon(@Param("isRecon") boolean type);
 	
+	
+	
+	List<Feed> findByTypeAndThreatType_MalwareAndConfidenceGreaterThan(@Param("type") String type, 
+			@Param("isMalware") boolean value,
+			@Param("confidenceLessThan") double confidence);
 
 	List<Feed> findByFirstSeenBefore(@Param("firstSeen") 
 	@DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss") LocalDateTime firstSeen);
